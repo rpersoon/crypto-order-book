635b140cdec8ffa1d966ab8983bd57fac9f14bee
Ricardo <git@ricardopersoon.nl>
2019-02-09 22:23:00 -0500

    Update requirements and setup

--- setup.py
+++ setup.py
@@ -6,8 +6,14 @@ from setuptools import setup
 
 setup(
    name='crypto_order_book',
-   version='1.0.0',
+   version='1.0.1',
    description='Cryptocurrency exchange order book',
    author='Ricardo Persoon',
-   packages=['crypto_order_book']
+   packages=['crypto_order_book'],
+   install_requires=[
+      # We need to pin sortedcontainers to a 1.x version, as an incompatible and unfinished 2.x version was released
+      # that is still missing functionality that we need
+      'sortedcontainers==1.5.10',
+      'websocket-client'
+   ]
 )
